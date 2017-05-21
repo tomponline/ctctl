4dced81dc7be70d36ccc00d627dfdbce96b13ebf
tomponline <tomp@tomp.uk>
2017-05-21 10:38:13 +0000

    various changes

--- ctctl-netup/ctctl-netup.go
+++ ctctl-netup/ctctl-netup.go
@@ -11,6 +11,7 @@ import (
 	"log/syslog"
 	"os/exec"
 	"strings"
+	"os"
 )
 
 var (
@@ -20,18 +21,28 @@ var (
 func init() {
 	flag.Parse()
 	log.SetFlags(0)
-	syslogWriter, err := syslog.New(syslog.LOG_INFO, "lxcnetup")
+	syslogWriter, err := syslog.New(syslog.LOG_INFO, "ctctl-netup")
 	if err == nil {
 		log.SetOutput(syslogWriter)
 	}
 }
 
+type ipAddr struct {
+        ip	 string
+        routeDev string
+}
+
+type proxyDev struct {
+        dev string
+        gwIps []string
+}
+
+
 func main() {
-	ctDevRef := flag.Arg(0)
-	ctName := flag.Arg(1)
-	context := flag.Arg(3)
-	devType := flag.Arg(4)
-	hostDevName := flag.Arg(5)
+	ctName := flag.Arg(0)
+	context := flag.Arg(2)
+	devType := flag.Arg(3)
+	hostDevName := flag.Arg(4)
 
 	c, err := lxc.NewContainer(ctName, lxc.DefaultConfigPath())
 	if err != nil {
@@ -49,42 +60,57 @@ func main() {
 	var v4Ips []string
 	var v6Ips []string
 
-	//Check ctDevRef in network up scripts
+        proxyDevs := make(map[string]proxyDev)
+
+	//Check for which network has correct up script
 	for i := 0; i < len(c.ConfigItem("lxc.network")); i++ {
 		upScript := c.ConfigItem(fmt.Sprintf("lxc.network.%d.script.up", i))
-		upEndArg := " " + ctDevRef
-		if strings.HasSuffix(upScript[0], upEndArg) {
+		//Check up script is this program.
+		if strings.HasSuffix(upScript[0], os.Args[0]) {
 			v4Ips = c.ConfigItem(fmt.Sprintf("lxc.network.%d.ipv4", i))
 			v6Ips = c.ConfigItem(fmt.Sprintf("lxc.network.%d.ipv6", i))
+                        
+			hostProxyDev := proxyDev{
+				dev: hostDevName,
+				gwIps: make([]string,0),
+			}
+			v4Gws := c.ConfigItem(fmt.Sprintf("lxc.network.%d.ipv4.gateway", i))
+			if v4Gws[0] != "" {
+				hostProxyDev.gwIps = append(hostProxyDev.gwIps,v4Gws[0])
+			}
+                        v6Gws := c.ConfigItem(fmt.Sprintf("lxc.network.%d.ipv6.gateway", i))
+			if v6Gws[0] != "" {
+                                hostProxyDev.gwIps = append(hostProxyDev.gwIps,v6Gws[0])
+                        }
+
+			proxyDevs[hostDevName] = hostProxyDev
 			break //Found what we needed
 		}
 	}
 
 	if len(v4Ips) <= 0 && len(v6Ips) <= 0 {
-		log.Print("No IPs defined for CT Dev Ref: ", ctDevRef)
-		return
+		log.Fatal("No IPs defined for CT Dev Ref: ", ctName)
 	}
 
+	if len(proxyDevs[hostDevName].gwIps) <= 0 {
+                log.Fatal("No Gateways defined for CT Dev Ref: ", ctName)
+        }
+
 	ipAddrs := make([]ipAddr, 0)
-	enrichIps(v4Ips, &ipAddrs)
-	enrichIps(v6Ips, &ipAddrs)
+	enrichIps(v4Ips, &ipAddrs, proxyDevs)
+	enrichIps(v6Ips, &ipAddrs, proxyDevs)
 
 	switch context {
 	case "up":
-		runUp(c, ctName, hostDevName, ipAddrs)
+		runUp(c, ctName, hostDevName, ipAddrs, proxyDevs)
 	case "down":
-		runDown(c, ctName, hostDevName, ipAddrs)
+		runDown(c, ctName, hostDevName, ipAddrs, proxyDevs)
 	default:
 		log.Fatal("Unknown context: ", context)
 	}
 }
 
-type ipAddr struct {
-	ip       string
-	routeDev string
-}
-
-func enrichIps(ips []string, outIps *[]ipAddr) {
+func enrichIps(ips []string, outIps *[]ipAddr, outDevs map[string]proxyDev) {
 	for _, ip := range ips {
 		//Convert to IP without netmask.
 		index := strings.Index(ip, "/")
@@ -106,10 +132,14 @@ func enrichIps(ips []string, outIps *[]ipAddr) {
 		parts := strings.Fields(string(stdoutStderr))
 		for index, val := range parts {
 			if val == "dev" {
+				devName := parts[index+1]
 				*outIps = append(*outIps, ipAddr{
 					ip:       ip,
-					routeDev: parts[index+1],
+					routeDev: devName,
 				})
+				outDevs[devName] = proxyDev{
+					dev: devName,
+				}
 				break
 			}
 		}
@@ -117,33 +147,30 @@ func enrichIps(ips []string, outIps *[]ipAddr) {
 	return
 }
 
-func runUp(c *lxc.Container, ctName string, hostDevName string, ips []ipAddr) {
+func activateProxyNdp(dev string) error {
+        //Enable proxy ndp on  interface (needed before adding specific proxy entries)
+        proxyNdpFile := "/proc/sys/net/ipv6/conf/" + dev + "/proxy_ndp"
+        return ioutil.WriteFile(proxyNdpFile, []byte("1"), 0644)
+}
+
+func runUp(c *lxc.Container, ctName string, hostDevName string, ips []ipAddr, proxyDevs map[string]proxyDev) {
 	log.Printf("LXC Net UP: %s %s %s", ctName, hostDevName, ips)
 
-	//Enable proxy arp on host interface
-	proxyArpFile := "/proc/sys/net/ipv4/conf/" + hostDevName + "/proxy_arp"
-	if err := ioutil.WriteFile(proxyArpFile, []byte("1"), 0644); err != nil {
-		log.Fatal("Error activating proxy arp on ", hostDevName, ": ", err)
-	}
-	log.Print("Activated proxy arp on ", hostDevName)
-
-	//Enable proxy ndp on host interface
-	proxyNdpFile := "/proc/sys/net/ipv6/conf/" + hostDevName + "/proxy_ndp"
-	if err := ioutil.WriteFile(proxyNdpFile, []byte("1"), 0644); err != nil {
-		log.Fatal("Error activating proxy ndp on ", hostDevName, ": ", err)
-	}
-	log.Print("Activated proxy ndp on ", hostDevName)
-
-	//Add proxy entries to host interface for gateway IPs.
-	proxyGateways := []string{"192.0.2.1", "fd60:eafa:54a9::1"}
-	for _, gwIp := range proxyGateways {
-		//Setup proxy arp for default IP route on host interface
-		cmd := exec.Command("ip", "neigh", "replace", "proxy", gwIp, "dev", hostDevName)
-		if stdoutStderr, err := cmd.CombinedOutput(); err != nil {
-			log.Fatal("Error adding proxy IP address '", gwIp, "': ", err, " ", string(stdoutStderr))
+	for _,proxyDev := range proxyDevs {
+		if err := activateProxyNdp(proxyDev.dev); err != nil {
+       	         log.Fatal("Error activating proxy ndp on ", proxyDev.dev, ": ", err)
 		}
-		log.Print("Added proxy for IP ", gwIp, " on ", hostDevName)
+		log.Print("Activated proxy ndp on ", proxyDev.dev)
+		
+	        for _, gwIp := range proxyDev.gwIps {
+        	        //Setup proxy arp for default IP route on host interface
+                	cmd := exec.Command("ip", "neigh", "replace", "proxy", gwIp, "dev", proxyDev.dev)
+                	if stdoutStderr, err := cmd.CombinedOutput(); err != nil {
+                        	log.Fatal("Error adding proxy IP address '", gwIp, "': ", err, " ", string(stdoutStderr))
+                	}
+                	log.Print("Added proxy for IP ", gwIp, " on ", proxyDev.dev)
 
+        	}
 	}
 
 	//Add static route and proxy entry for each IP
@@ -162,5 +189,5 @@ func runUp(c *lxc.Container, ctName string, hostDevName string, ips []ipAddr) {
 	}
 }
 
-func runDown(c *lxc.Container, ctName string, hostDevName string, ips []ipAddr) {
+func runDown(c *lxc.Container, ctName string, hostDevName string, ips []ipAddr, proxyDevs map[string]proxyDev) {
 }
